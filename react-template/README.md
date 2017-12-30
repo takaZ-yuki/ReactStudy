7f7e6a9aabf24c4915f1d910e7f219d90037ee74
yuki <gulgul.groove@gmail.com>
2017-12-31 05:44:50 +0900

    add firebase library add template project

---
+++ react-template/README.md
@@ -0,0 +1,22 @@
+# React + TypeScriptの Templateプロジェクト
+
+まず、github経由ｄでソースを落としてくる
+
+### npm経由でライブラリを落としてくる↓
+
+```
+  npm install
+```
+
+### ビルド方法↓
+
+```
+  npm run build
+```
+
+### 開発用webサーバ起動
+
+```
+  npm run start
+```
+
--- react-template/src/tsx/Hello.tsx
+++ react-template/src/tsx/Hello.tsx
@@ -1,8 +1,11 @@
-/// <reference types="react" />
 import * as React from 'react';
+
 export interface Props {
     content: string;
 }
+
 export default class MyComponent extends React.Component<Props, {}> {
-    render(): JSX.Element;
+    render() {
+        return <div className="red">{this.props.content}</div>
+    }
 }
